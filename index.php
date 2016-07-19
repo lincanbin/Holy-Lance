97d56c80d528ff50015dfe629a4b940fb5b6dc6f
lincanbin <lincanbin@hotmail.com>
2016-07-19 16:56:36 +0800

    Main page.

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Carbon-Probe
+ * https://github.com/lincanbin/Carbon-Probe
+ *
+ * Copyright 2016 Canbin Lin (lincanbin@hotmail.com)
+ * http://www.94cb.com/
+ *
+ * Licensed under the MIT License
+ * 
+ * A Linux environmental probe.
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Carbon Probe</title>
+	<link href="static/css/style.css" rel="stylesheet"/>
+</head>
+
+<body class="main">
+<div class="top-tab">
+<?php
+$output = null;
+exec('top -n 1', $output);
+var_dump($output);
+?>
+</div>
+
+<script type="text/javascript" src="static/js/jquery.min.js"></script>
+<script type="text/javascript" src="static/js/common.js"></script>
+</body>
+</html>
