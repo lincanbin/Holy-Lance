914376f042a0a21f3772f951b7654018b07dc5bb
lincanbin <lincanbin@hotmail.com>
2016-08-06 17:45:41 +0800

    Show real CPU usage.

--- static/js/common.js
+++ static/js/common.js
@@ -1,14 +1,46 @@
-$(document).ready(function () {
+/*
+ * Carbon-Probe
+ * https://github.com/lincanbin/Carbon-Probe
+ *
+ * Copyright 2016 Canbin Lin (lincanbin@hotmail.com)
+ * http://www.94cb.com/
+ *
+ * Licensed under the MIT License:
+ * https://opensource.org/licenses/MIT
+ * 
+ * A Linux environmental probe.
+ */
 
+function refreshChart() {
+	$.ajax({
+		type: "POST",
+		url: "api.php",
+		data: {
+			password: '12345678'
+		},
+		dataType: "json",
+		success: function(data){
+			axisData = (new Date()).toLocaleTimeString().replace(/^\D*/,'');
+			var chartData = cpuUsageChartoption.series[0].data;
+			chartData.shift();
+			chartData.push(data.cpu_usage);
+
+			cpuUsageChartoption.xAxis.data.shift();
+			cpuUsageChartoption.xAxis.data.push(axisData);
+
+			cpuUsageChart.setOption(cpuUsageChartoption);
+			setTimeout("refreshChart()",2000);
+		}
+	});
+}
+
+$(document).ready(function () {
 	$('#MainTab').easyResponsiveTabs({
 		type: 'default', //Types: default, vertical, accordion
 		width: 'auto', //auto or any width like 600px
 		fit: true, // 100% fit in a container
 		closed: 'accordion', // Start closed if in accordion view
-		tabidentify: 'main', // The tab groups identifier
-		activate: function (event) { // Callback function if tab is switched
-			console.log(event);
-		}
+		tabidentify: 'main' // The tab groups identifier
 	});
 
 	$('#PerformanceTab').easyResponsiveTabs({
@@ -22,8 +54,8 @@ $(document).ready(function () {
 		active_content_border_color: '#5AB1D0' // border color for active tabs contect in this group so that it matches the tab head border
 	});
 
-	var cpuUsageChart = echarts.init(document.getElementById('cpu_usage'));
-	var cpuUsageChartoption = {
+	window.cpuUsageChart = echarts.init(document.getElementById('cpu_usage'));
+	window.cpuUsageChartoption = {
 		title: {},
 		tooltip: {},
 		xAxis: {
@@ -44,6 +76,7 @@ $(document).ready(function () {
 			min: 0,
 			boundaryGap: [0.1, 0.1]
 		},
+		color: ['#117DBB'],
 		series: [
 			{
 				name:'CPU Usage',
@@ -61,17 +94,5 @@ $(document).ready(function () {
 			}
 		]
 	};
-	setInterval(function (){
-		axisData = (new Date()).toLocaleTimeString().replace(/^\D*/,'');
-
-		var data = cpuUsageChartoption.series[0].data;
-		data.shift();
-		data.push(Math.round(Math.random() * 100));
-
-		cpuUsageChartoption.xAxis.data.shift();
-		cpuUsageChartoption.xAxis.data.push(axisData);
-
-		cpuUsageChart.setOption(cpuUsageChartoption);
-	}, 2100);
-
+	refreshChart();
 });
