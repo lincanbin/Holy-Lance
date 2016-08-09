c1f3d0a12f851fa6157c02a143f1146b4dea2a17
lincanbin <lincanbin@hotmail.com>
2016-08-09 18:39:02 +0800

    Return disk active time and rate.

--- api.php
+++ api.php
@@ -11,6 +11,8 @@
  * 
  * A Linux environmental probe.
  */
+define('SAMPLING_TIME', 200000); // 200ms
+
 // load
 $system_info = array();
 $system_info['load'] = sys_getloadavg();
@@ -21,19 +23,37 @@ exec("uptime | awk -F ',' '{print $1 $2}' | awk -F 'up' '{print $2}'", $uptime);
 $system_info['uptime'] = $uptime[0];
 unset($uptime);
 
-// cpu: %
+// network
+$network_status = array();
+$network_usage = array();
+exec("cat /proc/net/dev | grep \":\" | awk '{print $1 $2 \":\"  $3 \":\" $10 \":\" $11}'", $network_status);
+foreach ($network_status as $eth) {
+	$eth = explode(":", $eth);
+	$network_card_name = $eth[0];
+	unset($eth[0]);
+	$network_usage[$network_card_name] = $eth;
+}
+$system_info['network'] = $network_usage;
+unset($network_status);
+unset($network_usage);
+
+// cpu: % and disk
 $cpu_usage1 = array();
+$cpu_usage2 = array();
+$disk_usage1 = array();
+$disk_usage2 = array();
 // Deprecated Code: Low performance
 // exec("top -b -n2 | grep \"Cpu(s)\"|tail -n 1 | awk '{print $2 + $4}'", $cpu_usage);
 exec("grep 'cpu ' /proc/stat | awk '{usage=($2+$3+$4+$5+$6+$7+$8+$9+$10)} END {print usage\"\\n\"$5}'", $cpu_usage1);
-// delay 100ms
-usleep(100000);
-$cpu_usage2 = array();
+exec("cat /proc/diskstats | grep \"da\" | head -n1 | awk -F 'da' '{print $2}' | awk '{print $3 \"\\n\" $4 \"\\n\" $7 \"\\n\" $8}'", $disk_usage1);
+usleep(SAMPLING_TIME);
+exec("cat /proc/diskstats | grep \"da\" | head -n1 | awk -F 'da' '{print $2}' | awk '{print $3 \"\\n\" $4 \"\\n\" $7 \"\\n\" $8}'", $disk_usage2);
 exec("grep 'cpu ' /proc/stat | awk '{usage=($2+$3+$4+$5+$6+$7+$8+$9+$10)} END {print usage\"\\n\"$5}'", $cpu_usage2);
-//var_dump(array_merge($cpu_usage1, $cpu_usage2));
 $system_info['cpu_usage'] = round((($cpu_usage2[0] - $cpu_usage1[0]) - ($cpu_usage2[1] - $cpu_usage1[1])) / ($cpu_usage2[0] - $cpu_usage1[0]) * 100, 1);
 unset($cpu_usage1);
 unset($cpu_usage2);
+unset($disk_usage1);
+unset($disk_usage2);
 
 // memory: KiB
 $memory_usage_total = array();
@@ -65,21 +85,6 @@ $memory_usage_swap_free = array();
 exec("free | grep \"Swap\" | awk '{print $4}'", $memory_usage_swap_free);
 $system_info['memory_usage_swap_free'] = $memory_usage_swap_free[0];
 unset($memory_usage_swap_free);
-// disk
-
-// network
-$network_status = array();
-$network_usage = array();
-exec("cat /proc/net/dev | grep \":\" | awk '{print $1 $2 \":\"  $3 \":\" $10 \":\" $11}'", $network_status);
-foreach ($network_status as $eth) {
-	$eth = explode(":", $eth);
-	$network_card_name = $eth[0];
-	unset($eth[0]);
-	$network_usage[$network_card_name] = $eth;
-}
-$system_info['network'] = $network_usage;
-unset($network_status);
-unset($network_usage);
 
 // process
 $process_list = array();
