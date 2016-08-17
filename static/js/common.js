2672f74fb7579c095141025de66e14a4febfaee1
lincanbin <lincanbin@hotmail.com>
2016-08-18 00:14:53 +0800

    Refactor code.

--- static/js/common.js
+++ static/js/common.js
@@ -16,37 +16,37 @@ var password = 12345678; // ms
 
 
 function cloneObject(obj) {
-    var copy;
+	var copy;
 
-    // Handle the 3 simple types, and null or undefined
-    if (null === obj || "object" != typeof obj) return obj;
+	// Handle the 3 simple types, and null or undefined
+	if (null === obj || "object" != typeof obj) return obj;
 
-    // Handle Date
-    if (obj instanceof Date) {
-        copy = new Date();
-        copy.setTime(obj.getTime());
-        return copy;
-    }
+	// Handle Date
+	if (obj instanceof Date) {
+		copy = new Date();
+		copy.setTime(obj.getTime());
+		return copy;
+	}
 
-    // Handle Array
-    if (obj instanceof Array) {
-        copy = [];
-        for (var i = 0, len = obj.length; i < len; i++) {
-            copy[i] = cloneObject(obj[i]);
-        }
-        return copy;
-    }
+	// Handle Array
+	if (obj instanceof Array) {
+		copy = [];
+		for (var i = 0, len = obj.length; i < len; i++) {
+			copy[i] = cloneObject(obj[i]);
+		}
+		return copy;
+	}
 
-    // Handle Object
-    if (obj instanceof Object) {
-        copy = {};
-        for (var attr in obj) {
-            if (obj.hasOwnProperty(attr)) copy[attr] = cloneObject(obj[attr]);
-        }
-        return copy;
-    }
+	// Handle Object
+	if (obj instanceof Object) {
+		copy = {};
+		for (var attr in obj) {
+			if (obj.hasOwnProperty(attr)) copy[attr] = cloneObject(obj[attr]);
+		}
+		return copy;
+	}
 
-    throw new Error("Unable to copy obj! Its type isn't supported.");
+	throw new Error("Unable to copy obj! Its type isn't supported.");
 }
 
 function KibibytesToSize(bytes) {
