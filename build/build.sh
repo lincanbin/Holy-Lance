a1a33138d05349274551b739936a1eec2d895669
lincanbin <lincanbin@hotmail.com>
2018-05-11 01:09:13 +0800

    Remove '\n'.

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "Enter the file name of the Holy Lance you want (Default: holy_lance): \n"
+echo "Enter the file name of the Holy Lance you want (Default: holy_lance): "
 read file_name
 file_name="${file_name:=holy_lance}"
-echo "Enter the password of the Holy Lance you want (No password is required by default): \n"
+echo "Enter the password of the Holy Lance you want (No password is required by default): "
 read pw
 pw="${pw:=}"
 php build -n $file_name -p $pw
