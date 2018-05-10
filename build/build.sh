cdf95fced234a4e8ce0ec9bc48ec5dc9eb7cb091
lincanbin <lincanbin@hotmail.com>
2018-05-11 01:07:47 +0800

    CRLF to LF.

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-echo -n "Enter the file name of the Holy Lance you want (Default: holy_lance): "
-read file_name || file_name='holy_lance'
+echo "Enter the file name of the Holy Lance you want (Default: holy_lance): \n"
+read file_name
 file_name="${file_name:=holy_lance}"
-echo -n "Enter the password of the Holy Lance you want (No password is required by default): "
+echo "Enter the password of the Holy Lance you want (No password is required by default): \n"
 read pw
+pw="${pw:=}"
 php build -n $file_name -p $pw
