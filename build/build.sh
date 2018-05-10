3f1672db3ec12c35cacd53a7a2c3dd4949d41c07
lincanbin <lincanbin@hotmail.com>
2018-05-11 00:10:32 +0800

    Add password support for the build system: typos.

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo -n "Enter the file name of the Holy Lance you want (Default: holy_lance): "
 read file_name || file_name='holy_lance'
-file_name"=${file_name:=holy_lance}"
+file_name="${file_name:=holy_lance}"
 echo -n "Enter the password of the Holy Lance you want (No password is required by default): "
 read pw
 php build -n $file_name -p $pw
